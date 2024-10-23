d6e4e8a8444356afa8d8a249b9434b6aed21fdf0
Lawrence Emeka <mrabarah@gmail.com>
2024-10-24 00:15:40 +0100

    add routes

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('posts.index');
-});
+})->name('home');
 
 Route::get('/register', function () {
     return view('auth.register');
-});
+})->name('register');
